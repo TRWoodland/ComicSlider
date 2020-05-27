211da508109520cec03e6da0006f9f643d81e1df
john-owen <john_owen@eml.cc>
2020-05-27 17:21:25 +0100

    First check-in of the ComicSliderExceptions

---
+++ ComicSliderExceptions.py
@@ -0,0 +1,19 @@
+class BadRequestError(ValueError):
+    def __init__(self, message):
+        super(BadRequestError, self).__init__("Bad Request: " + str(message))
+
+class NotFoundError(ValueError):
+    def __init__(self, message):
+        super(NotFoundError, self).__init__("Not Found: " + str(message))
+
+class InternalServerError(Exception):
+    def __init__(self, message):
+        super(InternalServerError, self).__init__("Internal Server Error: " + str(message))
+
+class GatewayTimeoutError(Exception):
+    def __init__(self, message):
+        super(GatewayTimeoutError, self).__init__("Gateway Timeout Error: " + str(message))
+
+class ForbiddenError(Exception):
+    def __init__(self, message):
+        super(ForbiddenError, self).__init__("Forbidden Error: " + str(message))
--- Readme.txt
+++ Readme.txt
@@ -1,2 +1,3 @@
 Hello This is not a drill.
 This is a text file!
+
