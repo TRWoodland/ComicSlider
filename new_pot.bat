4d00c41f33fb7528ee1832a7973906ccae21d5c6
john-owen <john_owen@eml.cc>
2020-05-28 12:30:04 +0100

    added shared credentials

--- new_pot.bat
+++ new_pot.bat
@@ -1,5 +1,7 @@
 @echo off
 
+SET AWS_SHARED_CREDENTIALS_FILE=AwsStuff/.credentials
+SET PYTHONPATH=%cd%;%PYTHONPATH%
 SET SCRIPT=AwsStuff/LambdaCmdLine.py
 
 py -u %SCRIPT% --create_new
--- update_pot.bat
+++ update_pot.bat
@@ -1,6 +1,6 @@
 @echo off
 
-
+SET AWS_SHARED_CREDENTIALS_FILE=AwsStuff/.credentials
 SET PYTHONPATH=%cd%;%PYTHONPATH%
 SET SCRIPT=AwsStuff/LambdaCmdLine.py
 
